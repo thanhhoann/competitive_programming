13d5de2dc6b962079fd6be9c5bff008c274e8c35
thanhhoan <pdth456@gmail.com>
2021-12-15 22:52:17 +0700

    add script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const commander = require("commander");
+const program = new commander.Command();
+const execSync = require("child_process").execSync;
+
+const date = new Date();
+
+program
+  .command("cf")
+  .option("-m, --message <message_via_commit>", "commit message")
+  .action((options) => {
+    const _add = execSync(`git add .`, { encoding: "utf-8" });
+    const _commit = execSync(
+      `git commit -m "add codeforces/${
+        options.message
+      }.cpp at ${date.toLocaleString()}."`,
+      {
+        encoding: "utf-8",
+      }
+    );
+    const _push = execSync(`git push origin main`);
+    console.log(_push);
+  });
+
+program.parse(process.argv);
