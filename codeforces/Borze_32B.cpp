a6712f41c6fa6db7f7e71330a23d6aed17714f4c
thanhhoan <pdth456@gmail.com>
2021-12-15 13:48:02 +0700

    add 32B/codeforces

---
+++ codeforces/Borze_32B.cpp
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) {
+  return os << '(' << p.first << ", " << p.second << ')';
+}
+
+template <typename... T> auto sum(T... args) { return (args + ...); }
+template <typename... T> void read(T &...args) { ((cin >> args), ...); }
+template <typename... T> void write(T &&...args) {
+  ((cout << args << " "), ...);
+}
+
+#define ul unsigned long
+#define ll long long
+#define pb push_back
+#define PI (3.14159265)
+#define F first
+#define S second
+
+#define db(x) cout << #x << " = " << x << endl;
+#define For(i, k, n) for (unsigned long i = k; i < n; i++)
+#define For_equal(i, k, n) for (unsigned long i = k; i <= n; i++)
+#define For_map(map) for (const auto &e : map)
+
+void solve() {
+  string s;
+  cin >> s;
+
+  vector<int> res;
+
+  For(i, 0, s.length()) {
+    if (s[i] == '.') {
+      res.pb(0);
+    } else if (s[i] == '-') {
+      if (s[i + 1] == '.')
+        res.pb(1);
+      else
+        res.pb(2);
+      i++;
+    }
+  }
+
+  For(i, 0, res.size()) cout << res[i];
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  solve();
+
+  return 0;
+}
