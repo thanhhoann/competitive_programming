0925822ccd1f5bfbbd6d238533b14f2b437a8f35
thanhhoan <pdth456@gmail.com>
2021-12-15 17:03:33 +0700

    add 275A/codeforces

--- codeforces/Lights_Out_275A.cpp
+++ codeforces/Lights_Out_275A.cpp
@@ -1,3 +1,21 @@
+/*
+ * I named it "Lights Out" Algorithm ( actually the algorithm is not mine, but I
+ * think having a name is pretty cool :) ) 
+ *
+ *  Steps:
+ *
+ * 1. Declare 2 matrices: initial[][] and total_light_pressed[][]
+ * 2. At the beginning, let the total_light_pressed[][] having the same elements as the initial[][]
+ * 3. For every element of two matrices:
+ *    total_light_pressed[i][j - 1] += initial[i][j];
+ *    total_light_pressed[i][j + 1] += initial[i][j];
+ *    total_light_pressed[i - 1][j] += initial[i][j];
+ *    total_light_pressed[i + 1][j] += initial[i][j];
+ * 4. For every element of the total_light_pressed[][]:
+ *    - If it's even, then print 0. Else, print 1
+ *
+ */
+
 #include <algorithm>
 #include <array>
 #include <chrono>
@@ -43,8 +61,29 @@ template <typename... T> void write(T &&...args) {
 void solve() {
   int n = 3;
   int A[10][10];
+  int res[10][10];
 
-  For(i, 0, n) For(j, 0, n) read(A[i][j]);
+  For_equal(i, 1, n) For_equal(j, 1, n) {
+    read(A[i][j]);
+    res[i][j] = A[i][j];
+  }
+
+  For_equal(i, 1, n) For_equal(j, 1, n) {
+    res[i][j - 1] += A[i][j];
+    res[i][j + 1] += A[i][j];
+    res[i - 1][j] += A[i][j];
+    res[i + 1][j] += A[i][j];
+  }
+
+  For_equal(i, 1, n) {
+    For_equal(j, 1, n) {
+      if (res[i][j] % 2)
+        cout << "0";
+      else
+        cout << "1";
+    }
+    cout << "\n";
+  }
 }
 
 int main() {
