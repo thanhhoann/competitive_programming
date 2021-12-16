6f043652d462d167df1832f131c7c646f1830702
thanhhoan <pdth456@gmail.com>
2021-12-16 15:55:58 +0700

    add codeforces/80A.cpp at 12/16/2021, 15:55:58.

---
+++ codeforces/80A.cpp
@@ -0,0 +1,102 @@
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) {
+  return os << '(' << p.first << ", " << p.second << ')';
+}
+
+template <typename... T> auto sum(T... args) { return (args + ...); }
+template <typename... T> void read(T &...args) { ((cin >> args), ...); }
+template <typename... T> void write(T &&...args) {
+  ((cout << args << " "), ...);
+}
+
+#define ul unsigned long
+#define ll long long
+#define pb push_back
+#define PI (3.14159265)
+#define F first
+#define S second
+
+#define db(x) cout << #x << " = " << x << endl;
+#define For(i, k, n) for (unsigned long i = k; i < n; i++)
+#define For_equal(i, k, n) for (unsigned long i = k; i <= n; i++)
+#define For_map(map) for (const auto &e : map)
+
+void solve() {
+  int n, m;
+  read(n, m);
+
+  /* for (int i = n + 1; i <= m; i++) { */
+  /* bool isPrime = true; */
+  /* for (int j = 2; j * j <= i; j++) { */
+  /* if (i % j == 0) { */
+  /* isPrime = false; */
+  /* break; */
+  /* } */
+  /* } */
+  /* if (isPrime) { */
+  /* db(i); */
+  /* cout << "NO"; */
+  /* return; */
+  /* } */
+
+  int min = m;
+  int current = 0;
+  bool is_m_prime = true;
+
+  for (int i = 2; i <= m / 2; i++) {
+    if (m % i == 0) {
+      is_m_prime = false;
+    }
+  }
+
+  for (int i = n + 1; i <= m; i++) {
+    bool isPrime = true;
+
+    for (int j = 2; j * j <= i; j++) {
+      if (i % j == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) {
+      current = i;
+      if (current < min)
+        min = current;
+    }
+  }
+
+  if (min == m && is_m_prime)
+    cout << "YES";
+  else
+    cout << "NO";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  solve();
+
+  return 0;
+}
