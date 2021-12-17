2fc8fd86f03e78afe7de18690f6e388179cd6aae
thanhhoan <pdth456@gmail.com>
2021-12-17 07:44:07 +0700

    add codeforces/bla.cpp at 12/17/2021, 07:44:07.

---
+++ script/main.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+import commander from "commander";
+const program = new commander.Command();
+
+import exec from "child_process";
+const execSync = exec.execSync;
+import boxen from "boxen";
+
+const date = new Date();
+
+program
+  .command("codeforces")
+  .option("-m, --message <message_via_commit>", "commit message")
+  .action((options) => {
+    const _add = execSync(`git add .`, { encoding: "utf-8" });
+    const _commit = execSync(
+      `git commit -m "add codeforces/${
+        options.message
+      }.cpp at ${date.toLocaleString()}."`,
+      {
+        encoding: "utf-8",
+      }
+    );
+    const _push = execSync(`git push origin main`);
+  });
+
+program
+  .command("advent_of_code")
+  .option("-m, --message <message_via_commit>", "commit message")
+  .action((options) => {
+    const _add = execSync(`git add .`, { encoding: "utf-8" });
+    const _commit = execSync(
+      `git commit -m "add advent_of_code/${
+        options.message
+      }.cpp at ${date.toLocaleString()}."`,
+      {
+        encoding: "utf-8",
+      }
+    );
+    const _push = execSync(`git push origin main`);
+    if (_push) console.log(boxen("unicorn", { padding: 1 }));
+  });
+
+program
+  .command("leetcode")
+  .option("-m, --message <message_via_commit>", "commit message")
+  .action((options) => {
+    const _add = execSync(`git add .`, { encoding: "utf-8" });
+
+    const _commit = execSync(
+      `git commit -m "add leetcode/${
+        options.message
+      }.cpp at ${date.toLocaleString()}."`,
+      {
+        encoding: "utf-8",
+      }
+    );
+    const _push = execSync(`git push origin main`);
+    if (_push) console.log(`PUSHED ${options.message} !`);
+  });
+
+program.parse(process.argv);
