a96675e4aeb60f570ad9bb2102a83fc16838b636
thanhhoan <pdth456@gmail.com>
2021-12-18 17:17:58 +0700

    add codeforces/71A.cpp at 12/18/2021, 5:17:58 PM.

--- codeforces/71A.cpp
+++ codeforces/71A.cpp
@@ -16,7 +16,6 @@
 #include <vector>
 
 using namespace std;
-
 template <typename A, typename B>
 ostream &operator<<(ostream &os, const pair<A, B> &p) {
   return os << '(' << p.first << ", " << p.second << ')';
