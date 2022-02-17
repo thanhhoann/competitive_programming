d2752c5bf35f6b6fc97d4e80286c9a33c80a062a
thanhhoan <pdth456@gmail.com>
2022-02-17 09:34:45 +0700

    tahosenu

--- codeforces/263A.cpp
+++ codeforces/263A.cpp
@@ -1,70 +1,44 @@
 #include <algorithm>
-#include <array>
-#include <chrono>
-#include <deque>
-#include <functional>
 #include <iostream>
-#include <map>
 #include <math.h>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <stack>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
-template <typename A, typename B>
-ostream &operator<<(ostream &os, const pair<A, B> &p) {
-  return os << '(' << p.first << ", " << p.second << ')';
-}
-
-template <typename... T> auto sum(T... args) { return (args + ...); }
-template <typename... T> void read(T &...args) { ((cin >> args), ...); }
-template <typename... T> void write(T &&...args) {
-  ((cout << args << " "), ...);
-}
-
-#define ul unsigned long
 #define ll long long
-#define pb push_back
-#define PI (3.14159265)
-#define F first
-#define S second
 
 #define db(x) cout << #x << " = " << x << endl;
-#define For(i, k, n) for (unsigned long i = k; i < n; i++)
-#define For_equal(i, k, n) for (unsigned long i = k; i <= n; i++)
-#define For_map(map) for (const auto &e : map)
 
 void solve() {
-  int A[100][100];
-  int save_i = 0, save_j = 0;
+  int A[6][6];
+  int pos_x = 0;
+  int pos_y = 0;
 
-  For_equal(i, 1, 5) For_equal(j, 1, 5) {
-    read(A[i][j]);
-    if (A[i][j] == 1) {
-      save_i = i;
-      save_j = j;
+  for (int i = 1; i <= 5; i++) {
+    for (int j = 1; j <= 5; j++) {
+      cin >> A[i][j];
+      if (A[i][j] == 1) {
+        pos_x = i;
+        pos_y = j;
+      }
     }
   }
 
-  int a = abs(3 - save_i);
-  int b = abs(3 - save_j);
-  int d = a + b;
+  int ans = 0;
 
-  cout << d;
+  if (pos_x != 3) {
+    ans = abs(pos_x - 3) + abs(pos_y - 3);
+  } else if (pos_x == 3) {
+    ans = abs(3 - pos_y);
+  } else if (pos_y == 3) {
+    ans = abs(3 - pos_x);
+  }
+
+  cout << ans;
 }
 
 int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
-
   solve();
-
   return 0;
 }
--- codeforces/791A.cpp
+++ codeforces/791A.cpp
@@ -1,52 +1,30 @@
 #include <algorithm>
-#include <array>
-#include <chrono>
-#include <deque>
-#include <functional>
 #include <iostream>
-#include <map>
 #include <math.h>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <stack>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
 using namespace std;
-
-template <typename... T> void read(T &...args) { ((cin >> args), ...); }
-template <typename... T> void write(T &&...args) {
-  ((cout << args << " "), ...);
-}
 
 #define ll long long
 
 #define db(x) cout << #x << " = " << x << endl;
 
 void solve() {
-  int l, b;
-  read(l, b);
+  int a, b;
+  cin >> a >> b;
 
-  int l_r = l, b_r = b, count = 0;
+  int count = 0;
 
-  while (l_r <= b_r) {
-    l_r *= 3;
-    b_r *= 2;
+  while (a <= b) {
+    a *= 3;
+    b *= 2;
     count++;
   }
-
   cout << count;
 }
 
 int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
-
   solve();
-
   return 0;
 }
